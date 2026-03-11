f187d24dc6897c81cf7d93c1ae6ed72f80c276a3
JOSE DOMINGOS DE JESUS SANTOS <jose.santos.cns@gmail.com>
2026-03-10 23:21:22 -0300

    Add files via upload

---
+++ SENHA.js
@@ -0,0 +1,11 @@
+// Admin (acesso total)
+Email: admin@techfix.com
+Senha: admin123
+
+// Editor (pode criar/editar)
+Email: editor@techfix.com
+Senha: editor123
+
+// Usuário comum (apenas visualização)
+Email: tecnico@techfix.com
+Senha: tecnico123
---
+++ mbvg.sh
@@ -0,0 +1,16 @@
+# Instalar Vercel CLI
+npm i -g vercel
+
+# Fazer deploy
+vercel
+
+# Seguir as instruções:
+# - Set up and deploy: Y
+# - Which scope: Selecione sua conta
+# - Link to existing project: N
+# - Project name: techfix-pro
+# - Directory: ./
+# - Override settings: N
+
+# Para produção:
+vercel --prod
